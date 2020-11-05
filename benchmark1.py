b7384e32a12ba8b5d461e73ec5e5f04b8a0c8b79
richard <yueyericardo@gmail.com>
2020-11-04 19:10:42 -0500

    gpu usage, nnpops c++ and more pdb file

--- benchmark1.py
+++ benchmark1.py
@@ -2,61 +2,109 @@ import mdtraj
 import time
 import torch
 import torchani
-
+import pynvml
+import gc
+import os
+import subprocess
+import argparse
 from NNPOps.SymmetryFunctions import TorchANISymmetryFunctions
 
-device = torch.device('cuda')
 
-mol = mdtraj.load('molecules/2iuz_ligand.mol2')
-species = torch.tensor([[atom.element.atomic_number for atom in mol.top.atoms]], device=device)
-positions = torch.tensor(mol.xyz, dtype=torch.float32, requires_grad=False, device=device)
-
-nnp = torchani.models.ANI2x(periodic_table_index=True, model_index=None).to(device)
-speciesPositions = nnp.species_converter((species, positions))
-symmFuncRef = nnp.aev_computer
-symmFunc = TorchANISymmetryFunctions(nnp.aev_computer).to(device)
-
-N = 4000
-
-aev_ref = symmFuncRef(speciesPositions).aevs
-torch.cuda.synchronize()
-start = time.time()
-for _ in range(N):
-    aev_ref = symmFuncRef(speciesPositions).aevs
-torch.cuda.synchronize()
-delta = time.time() - start
-print('Original TorchANI symmetry functions')
-print(f'  Duration: {delta:.2f} s')
-print(f'  Speed: {delta/N*1000:.3f} ms/it')
-print()
+def checkgpu(device=None):
+    i = device if device else torch.cuda.current_device()
+    real_i = int(os.environ['CUDA_VISIBLE_DEVICES'][0]) if 'CUDA_VISIBLE_DEVICES' in os.environ else i
+    pynvml.nvmlInit()
+    h = pynvml.nvmlDeviceGetHandleByIndex(real_i)
+    info = pynvml.nvmlDeviceGetMemoryInfo(h)
+    name = pynvml.nvmlDeviceGetName(h)
+    print('  GPU Memory Used (nvidia-smi): {:7.1f}MB / {:.1f}MB ({})'.format(info.used / 1024 / 1024, info.total / 1024 / 1024, name.decode()))
 
 
-aev = symmFunc(speciesPositions).aevs
-torch.cuda.synchronize()
-start = time.time()
-for _ in range(N):
-    aev = symmFunc(speciesPositions).aevs
-torch.cuda.synchronize()
-delta = time.time() - start
-print('Optimized TorchANI symmetry functions')
-print(f'  Duration: {delta:.2f} s')
-print(f'  Speed: {delta/N*1000:.3f} ms/it')
-aev_error = torch.max(torch.abs(aev - aev_ref))
-print(f'  Error: {aev_error:.1e}\n')
-assert aev_error < 0.02
+def alert(text):
+    print('\033[91m{}\33[0m'.format(text))  # red
 
 
-nnp.aev_computer.use_cuda_extension = True
-cuaev_computer = nnp.aev_computer
-torch.cuda.synchronize()
-start = time.time()
-for _ in range(N):
-    aev = cuaev_computer(speciesPositions).aevs
-torch.cuda.synchronize()
-delta = time.time() - start
-print('CUaev (Kamesh)')
-print(f'  Duration: {delta:.2f} s')
-print(f'  Speed: {delta/N*1000:.3f} ms/it')
-aev_error = torch.max(torch.abs(aev - aev_ref))
-print(f'  Error: {aev_error:.1e}   # note that this error is big, should be cuaevs bug to be fix\n')
-assert aev_error < 0.02
+def info(text):
+    print('\033[32m{}\33[0m'.format(text))  # green
+
+
+def benchmark(speciesPositions, aev_comp, N, check_gpu_mem):
+    torch.cuda.empty_cache()
+    gc.collect()
+    torch.cuda.synchronize()
+    start = time.time()
+
+    for i in range(N):
+        aev = aev_comp(speciesPositions).aevs
+        if i == 2 and check_gpu_mem:
+            checkgpu()
+
+    torch.cuda.synchronize()
+    delta = time.time() - start
+    print(f'  Duration: {delta:.2f} s')
+    print(f'  Speed: {delta/N*1000:.2f} ms/it')
+    return aev, delta
+
+
+def check_speedup_error(aev, aev_ref, speed, speed_ref):
+    speedUP = speed_ref / speed
+    if speedUP > 1:
+        info(f'  Speed up: {speedUP:.2f} X\n')
+    else:
+        alert(f'  Speed up (slower): {speedUP:.2f} X\n')
+
+    aev_error = torch.max(torch.abs(aev - aev_ref))
+    assert aev_error < 0.02, f'  Error: {aev_error:.1e}\n'
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--nnpops_cu_out',
+                        default=None)
+    parser.add_argument('-c', '--check_gpu_mem',
+                        dest='check_gpu_mem',
+                        action='store_const',
+                        const=1)
+    parser.set_defaults(check_gpu_mem=0)
+    parser = parser.parse_args()
+
+    check_gpu_mem = parser.check_gpu_mem
+    device = torch.device('cuda')
+    # files = ['2iuz_ligand.mol2', '1hvk_ligand.mol2', '2iuz_ligand.mol2', '3hkw_ligand.mol2', '3hky_ligand.mol2', '3lka_ligand.mol2', '3o99_ligand.mol2', 'small.pdb', '1hz5.pdb', '6W8H.pdb']
+    files = ['small.pdb', '1hz5.pdb', '6W8H.pdb']
+
+    for file in files:
+        mol = mdtraj.load(f'molecules/{file}')
+        species = torch.tensor([[atom.element.atomic_number for atom in mol.top.atoms]], device=device)
+        positions = torch.tensor(mol.xyz * 10, dtype=torch.float32, requires_grad=False, device=device)
+        print(f'File: {file}, Molecule size: {species.shape[-1]}\n')
+
+        nnp = torchani.models.ANI2x(periodic_table_index=True, model_index=None).to(device)
+        speciesPositions = nnp.species_converter((species, positions))
+        symmFuncRef = nnp.aev_computer
+        symmFunc = TorchANISymmetryFunctions(nnp.aev_computer).to(device)
+
+        N = 100
+
+        print('Original TorchANI:')
+        aev_ref, delta_ref = benchmark(speciesPositions, symmFuncRef, N, check_gpu_mem)
+        print()
+
+        print('NNPops:')
+        aev, delta = benchmark(speciesPositions, symmFunc, N, check_gpu_mem)
+        check_speedup_error(aev, aev_ref, delta, delta_ref)
+
+        if parser.nnpops_cu_out and file.endswith(".pdb"):
+            print('NNPops (C++ directly):')
+            commands = f"./{parser.nnpops_cu_out} molecules/{file} {N}"
+            subprocess.run(commands, shell=True, check=True, universal_newlines=True)
+            print()
+
+        print('CUaev (Kamesh):')
+        nnp.aev_computer.use_cuda_extension = True
+        cuaev_computer = nnp.aev_computer
+        aev, delta = benchmark(speciesPositions, cuaev_computer, N, check_gpu_mem)
+        check_speedup_error(aev, aev_ref, delta, delta_ref)
+
+        print('-'*70 + '\n')
